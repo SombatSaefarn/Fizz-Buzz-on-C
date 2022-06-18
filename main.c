dbbbd6e91bdb1553fb5ba795e7ddcb9daaa3f7fe
SombatSaefarn <97969697+SombatSaefarn@users.noreply.github.com>
2022-06-18 21:12:29 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+int fizzbuzz(int start,int end){
+    for (;start<end+1;++start){
+        if (start % 3 == 0) {
+            printf("%d %s\n",start, "fizz");
+        }
+        else if (start % 5 == 0) {
+            printf("%d %s\n",start, "buzz");
+        }
+        else if (start % 3 == 0 && start % 5 == 0) {
+            printf("%d %s\n",start, "fizzbuzz");
+        }
+        else {
+            printf("%d\n",start);
+        }
+    }
+}
+
+int main()
+{
+    fizzbuzz(1,50);
+    return 0;
+}
